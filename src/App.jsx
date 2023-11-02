abae79b86d19c7db2ffa31b3bb25b55491c2e735
Shoaib <mhshoaib839@gmail.com>
2023-11-02 15:09:38 +0500

    path issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import FleetTop from "./components/Fleet/FleetTop";
 import Cards from "./components/fleetcard/Cards";
-import Mainsection from "./components/mainsection/mainsection";
+// import Mainsection from "./components/mainsection/mainsection";
+import Mainsection from "./components/mainsection/Mainsection";
 import Services from "./components/services/Services";
 import ServicesCard from "./components/services/ServicesCard";
 function App() {
