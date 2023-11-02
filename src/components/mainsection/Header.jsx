c3cbb8b20aa945a4d2211a8eb6b6fe803ad7d7b8
Shoaib <mhshoaib839@gmail.com>
2023-11-02 18:40:47 +0500

    jj

--- src/components/mainsection/Header.jsx
+++ src/components/mainsection/Header.jsx
@@ -3,8 +3,10 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { BiPhoneCall } from "react-icons/bi";
 import Group34 from "../../assets/Group34.svg";
 import "./Header.css";
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
-  const [toggle, setISToggle] = useState("False");
+  const [toggle, setISToggle] = useState(false);
+
   return (
     <>
       <div className="navbar-container">
@@ -35,7 +37,6 @@ const Header = () => {
         <div className="navbar-container-3">
           <div className="phone">
             <p>
-              {" "}
               <BiPhoneCall />
               +92 333 0000111
             </p>
@@ -45,7 +46,47 @@ const Header = () => {
           </div>
         </div>
         <div className="main-hamburger">
-          <RxHamburgerMenu className="hamburger" />
+          {toggle ? (
+            // Show menu items and phone when toggle is true
+            <>
+              <div className="navbar-container-links--1">
+                <AiOutlineClose
+                  className="close"
+                  onClick={() => setISToggle(false)}
+                />
+                <p>
+                  <a href="#home">HOME</a>
+                </p>
+                <p>
+                  <a href="#fleet">FLEET</a>
+                </p>
+                <p>
+                  <a href="#service">SERVICES</a>
+                </p>
+                <p>
+                  <a href="#about">ABOUT US</a>
+                </p>
+                <p>
+                  <a href="#contact">CONTACT US</a>
+                </p>
+                <div className="phone--1">
+                  <p>
+                    <BiPhoneCall />
+                    +92 333 0000111
+                  </p>
+                </div>
+                <div className="button--nav--1">
+                  <button className="bt-nav--1">BOOK NOW</button>
+                </div>
+              </div>
+            </>
+          ) : (
+            // Show hamburger icon when toggle is false
+            <RxHamburgerMenu
+              className="hamburger"
+              onClick={() => setISToggle(true)} // Clicking the hamburger sets toggle to true
+            />
+          )}
         </div>
       </div>
     </>
